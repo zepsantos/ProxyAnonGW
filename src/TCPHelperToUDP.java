7261fa3d68c38d2f7d058a71955463b1e3dbca8c
josepgrs <josepgrs@gmail.com>
2020-05-24 08:33:05 +0100

    Melhorias

--- src/TCPHelperToUDP.java
+++ src/TCPHelperToUDP.java
@@ -4,27 +4,49 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class TCPHelperToUDP {
-
     public static void sendTCPPackagesToUDP(InputStream is, InetAddress address, int port) throws IOException {
         Logger.getLogger(TCPHelperToUDP.class.getName()).info("Sending TCP packages to UDP to " + address.getHostAddress() + " on port(UDP):  " + port);
 
-        byte[] buffer = new byte[500];
-        int packagesCount = 0;
+        byte[] buffer = new byte[65535];
+
         int bytesRead = 0;
         while((bytesRead = is.read(buffer,0,buffer.length)) != -1) {
-            UDPData data = new UDPData(packagesCount,buffer,bytesRead);
-            if(bytesRead < buffer.length) {
-                data.setFinalPacket();
-            }
+            List<UDPData> tmp = divideTCPPacketToUDPPackets(buffer,bytesRead);
+            dispatchUDPPackages(tmp,address,port);
+        }
+    }
+
+    private static List<UDPData> divideTCPPacketToUDPPackets(byte[] tcpContent,int bytesRead) {
+        int numPackages = 65535/500;
+        int bytesProcessed = 0;
+        int packagesCount = 0;
+        List<UDPData> dataUDPtoSend = new ArrayList<>();
+        while(numPackages > 0) {
+            int bytesToProcess = Math.min(500,bytesRead-bytesProcessed);
+            UDPData tmp = new UDPData(packagesCount,Arrays.copyOfRange(tcpContent,bytesProcessed,bytesProcessed + bytesToProcess),bytesProcessed);
+            dataUDPtoSend.add(tmp);
+            bytesProcessed += bytesToProcess;
+            numPackages--;
+            packagesCount++;
+
+        }
+
+        return dataUDPtoSend;
+    }
+
+    private static void dispatchUDPPackages(List<UDPData> dataUDPtoSend,InetAddress address, int port) throws IOException {
+        for(UDPData data : dataUDPtoSend) {
             byte[] bufToSend = ObjectSerializer.getObjectInByte(data);
-            if(bufToSend != null) {
+            if (bufToSend != null) {
                 DatagramSocket udpSocket = new DatagramSocket();
-                DatagramPacket datagramPacket = new DatagramPacket(bufToSend,bufToSend.length, address ,port);
+                DatagramPacket datagramPacket = new DatagramPacket(bufToSend, bufToSend.length, address, port);
                 udpSocket.send(datagramPacket);
-                packagesCount++;
             }
         }
     }
